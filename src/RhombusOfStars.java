1f0a65bde6dc5632c125794f7fab8f3429a23251
simovanev <simovanev@abv.bg>
2023-02-23 04:24:02 +0200

    OOP

--- src/RhombusOfStars.java
+++ src/RhombusOfStars.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class RhombusOfStars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        for (int i = 1; i <= n; i++) {
+            printRow(i, n);
+        }
+        for (int i = n - 1; i > 0; i--) {
+            printRow(i,n);
+        }
+    }
+
+    private static void printRow(int i, int n) {
+        for (int j = 0; j < n - i; j++) {
+            System.out.print(" ");
+        }
+        for (int j = 0; j < i; j++) {
+            System.out.print("*");
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
 }
