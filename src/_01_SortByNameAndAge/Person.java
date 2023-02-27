e3100994c527038e4022712a6aedcd924363b29d
simovanev <simovanev@abv.bg>
2023-02-27 03:48:40 +0200

    Encapsulation

---
+++ src/_01_SortByNameAndAge/Person.java
@@ -0,0 +1,26 @@
+package _01_SortByNameAndAge;
+
+ class Person {
+    private String firstName;
+    private String lastName;
+    private  int age;
+
+     Person(String firstName, String secondName, int age) {
+        this.firstName = firstName;
+        this.lastName = secondName;
+        this.age = age;
+    }
+
+      String getFirstName() {
+         return firstName;
+     }
+
+      int getAge() {
+         return age;
+     }
+
+     @Override
+     public String toString() {
+         return String.format("%s %s is %d years old.",firstName,lastName,age);
+     }
+ }
