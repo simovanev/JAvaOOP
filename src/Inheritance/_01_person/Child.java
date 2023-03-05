8efbdc31e71485c8e5fcb9ef19af13102310526c
simovanev <simovanev@abv.bg>
2023-03-05 14:45:48 +0200

    Encapsulation

---
+++ src/Inheritance/_01_person/Child.java
@@ -0,0 +1,7 @@
+package Inheritance._01_person;
+
+public class Child extends Person{
+    public Child(String name, int age) {
+        super( name, age);
+    }
+}
---
+++ src/Inheritance/_01_person/Person.java
@@ -0,0 +1,26 @@
+package Inheritance._01_person;
+
+public class Person {
+    private  String name;
+    private int age;
+    public Person( String name, int age){
+        this.name= name;
+        this.age=age;
+
+    }
+    public String getName(){
+        return this.name;
+
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public void setName(String name){
+        this.name= name;
+    }
+    public void setAge(int age){
+        this.age= age;
+    }
+
+
+}
