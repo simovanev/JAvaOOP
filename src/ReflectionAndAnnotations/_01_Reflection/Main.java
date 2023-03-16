20f6ed24e9a79b3c074e67f2759331a89b3320d9
simovanev <simovanev@abv.bg>
2023-03-16 04:26:38 +0200

    Encapsulation

---
+++ src/ReflectionAndAnnotations/_01_Reflection/Main.java
@@ -0,0 +1,18 @@
+package ReflectionAndAnnotations._01_Reflection;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class reflection= Reflection.class;
+        System.out.println(reflection);
+        Class superClass= reflection.getSuperclass();
+        System.out.println(superClass);
+        Class[] interfaces= reflection.getInterfaces();
+        for (Class intr:interfaces){
+            System.out.println(intr);
+        }
+        Object reflectionObject=reflection.getDeclaredConstructor().newInstance();
+        System.out.println(reflectionObject);
+    }
+}
