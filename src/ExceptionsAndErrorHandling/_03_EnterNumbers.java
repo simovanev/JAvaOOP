ba09f71e1289f940f70e493583c9f76f2a16db8a
simovanev <simovanev@abv.bg>
2023-03-22 05:13:38 +0200

    Encapsulation

--- src/ExceptionsAndErrorHandling/_03_EnterNumbers.java
+++ src/ExceptionsAndErrorHandling/_03_EnterNumbers.java
@@ -12,11 +12,14 @@ public class _03_EnterNumbers {
         int startNumber=1;
         int endNumber=100;
 
-        while (numberList.size() < 10) {
+        while (numberList.size()!=10) {
             try{
             int currentNumber = checkIfNumber(input);
             if (numberInRange(currentNumber,startNumber,endNumber)){
                 numberList.add(currentNumber);
+                if (numberList.size()==10){
+                    break;
+                }
                 startNumber=currentNumber;
 
                 input= scanner.nextLine();
@@ -26,14 +29,20 @@ public class _03_EnterNumbers {
                 System.out.println(e.getMessage());
             input= scanner.nextLine();
             }
-        }
+
+            }
+
 //todo printing
+        for (int i = 0; i < 9; i++) {
+            System.out.print(numberList.get(i)+", ");
+        }
+        System.out.println(numberList.get(9));
     }
 
     private static boolean numberInRange(int currentNumber,int start, int end) {
          if( currentNumber>start && currentNumber<end){
             return true;
-        }else throw new IllegalArgumentException(String.format("Your number is not in range (%d - 100)\n",start));
+        }else throw new IllegalArgumentException(String.format("Your number is not in range %d - 100!",start));
     }
 
     private static int checkIfNumber(String input) {
